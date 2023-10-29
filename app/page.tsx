96cd2ac88d2990cc2df6c69f59ca880de495c495
Jamie <www.jamie.top@gmail.com>
2023-10-29 10:41:02 +0800

    Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { lusitana } from "@/app/ui/fonts";
+import Image from "next/image";
 
 import Link from "next/link";
 export default function Page() {
@@ -18,8 +19,8 @@ export default function Page() {
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
-            <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
           </p>
+          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"></div>
           <h1 className="text-blue-500">I'm blue!</h1>
           <Link
             href="/login"
@@ -30,6 +31,13 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
         </div>
       </div>
     </main>
